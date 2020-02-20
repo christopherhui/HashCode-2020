78c15a97aa24a282b5e8f94c7c61d9362b0450bd
Christopher Hui <christophergkhui@gmail.com>
2020-02-20 11:49:28 -0800

    fixes

--- solution.cpp
+++ solution.cpp
@@ -88,8 +88,6 @@ void output() {
     }
 }
 
-
-
 int main() {
     vector<string> inputs = { "a_example.txt", "b_read_on.txt", "c_incunabula.txt",
                               "d_tough_choices.txt", "e_so_many_books.txt", "f_libraries_of_the_world.txt" };
